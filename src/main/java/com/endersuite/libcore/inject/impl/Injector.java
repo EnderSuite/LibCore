761b0377118f64d0fbfcd0c9ac24d8e384701ad2
maximilianheidenreich <github@maximilian-heidenreich.de>
2021-05-11 17:01:57 +0200

    Adds docs for Injector and moved from File to Path parameters

--- src/main/java/com/endersuite/libcore/inject/impl/Injector.java
+++ src/main/java/com/endersuite/libcore/inject/impl/Injector.java
@@ -2,6 +2,7 @@ package com.endersuite.libcore.inject.impl;
 
 import java.io.File;
 import java.io.InputStream;
+import java.nio.file.Path;
 import java.util.List;
 
 /**
@@ -10,12 +11,56 @@ import java.util.List;
  */
 public interface Injector {
 
-    boolean inject(File target, boolean stopOnError);
+    /**
+     * Injects all files inside the specified folder into the classpath.
+     *
+     * @param depsFolder
+     *          The folder containing the jar files
+     * @return
+     *          {@code true} if all files were injected successfully | {@code false} if not
+     */
+    void inject(Path depsFolder);
 
-    boolean download(InputStream urlTextStream, File target, boolean keepExisting);
+    /**
+     * Downloads files from urls specified in the urlTextStream.
+     *
+     * @param urlTextStream
+     *          The stream containing the urls (newline separated)
+     * @param targetFolder
+     *          The folder inside which the downloaded files will be saved
+     * @param override
+     *          Whether to override existing files
+     * @return
+     *          {@code true} if everything was downloaded | {@code false} if not
+     */
+    void download(InputStream urlTextStream, Path targetFolder, boolean override);
 
-    boolean download(File urlFile, File target, boolean keepExisting);
+    /**
+     * Downloads dependencies from url inside a file (newline separated).
+     *
+     * @param urlFile
+     *          The file inside which the urls are stored
+     * @param targetFolder
+     *          THe folder in which to save the downloaded files
+     * @param override
+     *          Whether to overwrite existing files
+     * @return
+     *          {@code true} if everything succeeded | {@code false} if there was an exception
+     */
+    void download(File urlFile, Path targetFolder, boolean override);
 
-    boolean download(List<String> urls, File target, boolean keepExisting);
+    /**
+     * Downloads dependencies from url list.
+     *
+     * @param urls
+     *          The urls at which the jar file are located
+     * @param targetFolder
+     *          THe folder in which to save the downloaded files
+     * @param override
+     *          Whether to overwrite existing files
+     * @return
+     *          {@code true} if everything succeeded | {@code false} if there was an exception
+     */
+    void download(List<String> urls, Path targetFolder, boolean override);
 
 }
