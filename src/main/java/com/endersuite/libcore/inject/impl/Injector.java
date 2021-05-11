7cb8d54c02d423c53386f1c6669e6fe1aafc84f9
maximilianheidenreich <github@maximilian-heidenreich.de>
2021-05-11 11:29:37 +0200

    Adds docs for Injector

--- src/main/java/com/endersuite/libcore/inject/impl/Injector.java
+++ src/main/java/com/endersuite/libcore/inject/impl/Injector.java
@@ -9,10 +9,44 @@ import java.util.List;
  */
 public interface Injector {
 
-    boolean inject(File target, boolean stopOnError);
+    /**
+     * Injects all files inside the specified folder into the classpath.
+     *
+     * @param depsFolder
+     *          The folder containing the jar files
+     * @param stopOnError
+     *          Whether to stop injecting on error
+     * @return
+     *          {@code true} if all files were injected successfully | {@code false} if not
+     */
+    boolean inject(File depsFolder, boolean stopOnError);
 
-    boolean download(File urlFile, File target, boolean keepExisting);
+    /**
+     * Downloads dependencies from url inside a file (newline separated).
+     *
+     * @param urlFile
+     *          The file inside which the urls are stored
+     * @param depsFolder
+     *          THe folder in which to save the downloaded files
+     * @param keepExisting
+     *          Whether to overwrite existing files
+     * @return
+     *          {@code true} if everything succeeded | {@code false} if there was an exception
+     */
+    boolean download(File urlFile, File depsFolder, boolean keepExisting);
 
-    boolean download(List<String> urls, File target, boolean keepExisting);
+    /**
+     * Downloads dependencies from url list.
+     *
+     * @param urls
+     *          The urls at which the jar file are located
+     * @param depsFolder
+     *          THe folder in which to save the downloaded files
+     * @param keepExisting
+     *          Whether to overwrite existing files
+     * @return
+     *          {@code true} if everything succeeded | {@code false} if there was an exception
+     */
+    boolean download(List<String> urls, File depsFolder, boolean keepExisting);
 
 }
