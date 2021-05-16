6e4912051995f4c28d1f54b19ed55ef888fc92d0
Dominik Klein <dominik@klein-kerpen.de>
2021-05-16 12:10:43 +0200

    refactored things

--- src/main/java/com/endersuite/libcore/serializer/ItemStackSerializer.java
+++ src/main/java/com/endersuite/libcore/serializer/ItemStackSerializer.java
@@ -2,12 +2,16 @@ package com.endersuite.libcore.serializer;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import lombok.*;
 import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.io.Serializable;
-import java.util.Map;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author TheRealDomm
@@ -22,42 +26,45 @@ public class ItemStackSerializer implements Serializable {
 
     private static final Gson GSON = new GsonBuilder().create();
 
-    private String material;
-    private String displayName;
-    private int amount;
-    private int slot;
-    private Map<String, Object> materialAttributes;
-
     public static String serialize(int slot, ItemStack itemStack) {
-        ItemStackSerializer serializer = new ItemStackSerializer();
+        List<SerializedItem> serializedItems = new ArrayList<>();
+        serializedItems.add(getSerializedItem(slot, itemStack));
+        return serialize(serializedItems);
+    }
+
+    public static String serialize(Inventory inventory) {
+        List<SerializedItem> serializedItems = new ArrayList<>();
+        for (int i = 0; i < inventory.getSize(); i++) {
+            if (inventory.getItem(i) != null && !inventory.getItem(i).getType().equals(Material.AIR)) {
+                ItemStack itemStack = inventory.getItem(i);
+                serializedItems.add(getSerializedItem(i, itemStack));
+            }
+        }
+        return serialize(serializedItems);
+    }
+
+    public static String serialize(List<SerializedItem> serializedItems) {
+        return GSON.toJson(serializedItems);
+    }
+
+    public static List<SerializedItem> deserializeInventory(String serialized) {
+        try {
+            Type items = new TypeToken<List<SerializedItem>>(){}.getType();
+            return GSON.fromJson(serialized, items);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private static SerializedItem getSerializedItem(int slot, ItemStack itemStack) {
+        SerializedItem serializer = new SerializedItem();
         serializer.setMaterial(itemStack.getType().name());
         serializer.setDisplayName(itemStack.getItemMeta().hasDisplayName() ?
                 itemStack.getItemMeta().getDisplayName() : null);
         serializer.setAmount(itemStack.getAmount());
         serializer.setSlot(slot);
         serializer.setMaterialAttributes(itemStack.serialize());
-        return GSON.toJson(serializer);
-    }
-
-    public ItemStackSerializer getInstance(String serialized) {
-        return GSON.fromJson(serialized, ItemStackSerializer.class);
-    }
-
-    public ItemStack toItemStack() {
-        return ItemStack.deserialize(this.materialAttributes);
-    }
-
-    public boolean isCompatible() {
-        try {
-            Material.valueOf(this.material);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public boolean hasDisplayName() {
-        return this.displayName != null;
+        return serializer;
     }
 
 }
---
+++ src/main/java/com/endersuite/libcore/serializer/SerializedItem.java
@@ -0,0 +1,44 @@
+package com.endersuite.libcore.serializer;
+
+import lombok.*;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * @author TheRealDomm
+ * @since 16.05.2021
+ */
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class SerializedItem implements Serializable {
+
+    private String material;
+    private String displayName;
+    private int amount;
+    private int slot;
+    private Map<String, Object> materialAttributes;
+
+    public ItemStack toItemStack() {
+        return ItemStack.deserialize(this.materialAttributes);
+    }
+
+    public boolean isCompatible() {
+        try {
+            Material.valueOf(this.material);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean hasDisplayName() {
+        return this.displayName != null;
+    }
+
+}
