2b1093bc62d8ffbc8c9ac6ee773fc069e1c03059
Dominik Klein <dominik@klein-kerpen.de>
2021-05-16 15:08:45 +0200

    removed all the stuff..., didnt work :(

--- src/main/java/com/endersuite/libcore/serializer/SerializedItem.java
+++ src/main/java/com/endersuite/libcore/serializer/SerializedItem.java
@@ -2,14 +2,10 @@ package com.endersuite.libcore.serializer;
 
 import com.endersuite.libcore.strfmt.StrFmt;
 import lombok.*;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import java.io.Serializable;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -33,31 +29,6 @@ public class SerializedItem implements Serializable {
     public ItemStack toItemStack() {
         new StrFmt(materialAttributes.toString()).toLog();
         ItemStack itemStack = ItemStack.deserialize(this.materialAttributes);
-        if (this.materialAttributes.containsKey("meta") && this.materialAttributes.get("meta") instanceof Map) {
-            try {
-                String packageName = Bukkit.getServer().getClass().getPackage().getName();
-                String version = packageName.substring(packageName.lastIndexOf(".") + 1);
-                try {
-                    Class.forName("org.bukkit.craftbukkit." + version + ".inventory.SerializableMeta");
-                    System.out.println("Yep, worked :)");
-                } catch (Exception e) {
-                    System.out.println("Not found :(");
-                }
-                /*Class<?> craftItem = Class.forName("org.bukkit.craftbukkit." + version + ".inventory.CraftItemStack");
-                Method asCraftCopy = craftItem.getMethod("asCraftCopy", ItemStack.class);
-                Object craftItemStackObject = asCraftCopy.invoke(null, itemStack);
-                Method getItemMeta = craftItem.getDeclaredMethod("getItemMeta");
-                Object craftItemMeta = getItemMeta.invoke(craftItemStackObject);*/
-                Class<?> craftMetaItemClass = Class.forName("org.bukkit.craftbukkit." + version + ".inventory.$CraftMetaItem.SerializableMeta");
-                Method deserialize = craftMetaItemClass.getMethod("deserialize", Map.class);
-                final Object invoke = deserialize.invoke(null, this.materialAttributes);
-                itemStack.setItemMeta((ItemMeta) invoke);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("No meta instance found!!");
-        }
         /*if (materialAttributes.containsKey("meta")) {
             itemStack.setItemMeta((ItemMeta) materialAttributes.get("meta"));
         }*/
